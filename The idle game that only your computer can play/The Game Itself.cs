3a3634bc6d5fdcbdb34eee8f7b399af24c4480ee
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2018-12-06 14:38:51 -0500

    Starting this thing up!
    Nothing to declare...

---
+++ The idle game that only your computer can play/The Game Itself.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_idle_game_that_only_your_computer_can_play
+{
+    class TheGameItself
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
