b1294511356ccecd7530265b4097ad84365f75c2
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2018-12-08 19:40:19 -0500

    Organisation Part 3
    Starting the integration of ressources (only the class is created for now...)

---
+++ The idle game that only your computer can play/Ressources/Ressource class.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace The_idle_game_that_only_your_computer_can_play.Text_Documents
+{
+    public class Ressource
+    {
+        readonly string name;
+        long level;
+        readonly long basePrice;
+        readonly long amountPerLevel;
+        readonly long sellPrice;
+        long amountProduced;
+        readonly List<(Ressource, long)> dependency;
+        long nextUpgradePrice;
+    }
+}
